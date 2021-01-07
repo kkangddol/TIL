1ef3c0392f948b141f9afc247777d97059fab74d
rudeore333 <knemo333@gmail.com>
2021-01-07 12:15:53 +0900

    doit 커밋 확인

---
+++ Kotlin/Do it! 코틀린 프로그래밍/KotlinProgramming/src/chap02/section1/UserClassImport.kt
@@ -0,0 +1,9 @@
+package chap02.section1
+
+import com.example.edu.Person
+
+fun main() {
+    val user1 = Person("babo", 33)
+    println(user1.name)
+    println(user1.age)
+}
---
+++ Kotlin/Do it! 코틀린 프로그래밍/KotlinProgramming/src/chap02/section1/defaultPackage.kt
@@ -0,0 +1,14 @@
+package chap02.section1
+
+//import kotlin.math.*
+//import java.lang.Math.*
+
+fun main() {
+    val intro: String = "안녕하세용"
+    val num: Int = 20
+
+    println(Math.PI)
+    println(Math.abs(-12.6))
+
+    println("intro : $intro , num : $num")
+}
