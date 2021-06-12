c4e9a4e9f8a9ff78bbd488f814613cb945801f91
rudeore <33540843+rudeore333@users.noreply.github.com>
2021-06-12 13:51:03 +0900

    14줄 개선 및 num 개선 -> 함수로 나눠서 main을 깔끔하게? 가독성이 좋게?
    코테는 제한된 시간때문에 로직에 집중하느라 구조까지 면밀히 고려하지 않았다.

--- Algorithm/codes/현우의모래시계.cpp
+++ Algorithm/codes/현우의모래시계.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int num = 10 - (2 * (n + 1) * (n + 1) - 1) % 10;
+	int num = (2 * (n + 1) * (n + 1) - 1) - 1;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i; j++) {
 			cout << ' ';
 		}
-		for (int k = i; k < 2 * n - i +1; k++) {
-			cout << 9 - (num % 10);
-			num++;
+		for (int k = 0; k < (2*n) + 1 - 2*i; k++) {
+			cout << num % 10;
+			num--;
 		}
 		cout << '\n';
 	}
@@ -21,19 +21,17 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cout << ' ';
 	}
-	cout << 9 - (num % 10) << endl;
-	num++;
+	cout << num % 10 << endl;
+	num--;
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 0; j < n-i; j++) {
 			cout << ' ';
 		}
 		for (int k = 0; k < 2 * i + 1; k++) {
-			cout << 9 - (num % 10);
-			num++;
+			cout << num % 10;
+			num--;
 		}
 		cout << '\n';
 	}
-
-		
 }
