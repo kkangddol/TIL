901a641dbeadb1a18f2eeade3948db8a3a138f43
rudeore <33540843+rudeore333@users.noreply.github.com>
2021-12-02 17:21:58 +0900

    좌표압축 너무 어렵게 생각하지좀 말자
    시간이랑 메모리 잘 확인하고 정수 범위내로 해결할 수 있으니까 좀 정신차리고
    어렵게 생각좀 하지마
    그냥 단순하게 생각해

--- Algorithm/codes/18870.cpp
+++ Algorithm/codes/18870.cpp
@@ -1,54 +1,36 @@
 #include <iostream>
-#include <limits>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-struct lowerBound {
-	long long value;
-	int order = 0;
-	lowerBound* lastPointer = this;
-} arr[1000000];
-
 int main() {
+	vector<pair<int, int>> v;
+	int ans[1000000] = {0,};
+	
 	int n;
 	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back({temp,i});
+	}
+
+	sort(v.begin(), v.end());
+
+	int pivot = v[0].first;
+	int cnt = 0;
 
 	for (int i = 0; i < n; i++) {
-		long long temp;
-		cin >> temp;
-		arr[i].value = temp;
-		if (i == 0) arr[i].order = 0;
-
-		long long maxValue = LLONG_MIN;
-		long long minValue = LLONG_MAX;
-
-		int tempIndex = -1;
-
-		for (int j = 0; j < i; j++) {
-			if (arr[j].value == temp) {
-				arr[i] = arr[j];
-				continue;
-			}
-			else if (arr[j].value < temp) {
-				if (arr[j].value > maxValue) {
-					maxValue = arr[j].value;
-					arr[i].lastPointer = &arr[j];
-				}
-			}
-			else {
-				if (arr[j].value < minValue) {
-					minValue = arr[j].value;
-					tempIndex = j;
-				}
-			}
+		if (pivot == v[i].first) {
+			ans[v[i].second] = cnt;
 		}
-		if (tempIndex != -1) {
-			arr[tempIndex].lastPointer = &arr[i];
+		else {
+			ans[v[i].second] = ++cnt;
+			pivot = v[i].first;
 		}
-		
 	}
 
 	for (int i = 0; i < n; i++) {
-		arr[i].order = arr[i].lastPointer->order + 1;
-		cout << arr[i].order << ' ';
+		cout << ans[i] << " ";
 	}
 }
