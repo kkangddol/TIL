5b8aa53dd15fbcd5b3f94290fedaf22c1f8b9d7d
rudeore <33540843+rudeore333@users.noreply.github.com>
2021-12-07 18:39:17 +0900

    N과 M (2)

---
+++ Algorithm/codes/15650.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int arr[9] = { 0, };
+int n, m;
+
+void output() {
+	int order = 1;
+	for (int i = 1; i <= n; i++) {
+		if (arr[i] == order) {
+			printf("%d ",i);
+			order++;
+			i = 0;
+			continue;
+		}
+	}
+	printf("\n");
+}
+
+void marking(int order, int index) {
+	if (order > m) {
+		output();
+		return;
+	}
+	
+	for (int i = index; i <= n; i++) {
+		if (arr[i] == 0) {
+			arr[i] = order;
+			marking(order + 1 , i + 1);
+			arr[i] = 0;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	marking(1,1);
+}
