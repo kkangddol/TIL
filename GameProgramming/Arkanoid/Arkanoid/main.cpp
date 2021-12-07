86a7e2e8b104e9cbf158106b01945ba27c73078b
rudeore333 <knemo333@gmail.com>
2021-12-07 17:08:59 +0900

    Arkanoid Copy Complete

--- GameProgramming/Arkanoid/Arkanoid/main.cpp
+++ GameProgramming/Arkanoid/Arkanoid/main.cpp
@@ -2,7 +2,14 @@
 #include <time.h>
 using namespace sf;
 
+bool isCollide(Sprite s1, Sprite s2)
+{
+	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
+
 int main() {
+	srand(time(0));
+
 	RenderWindow app(VideoMode(520, 450), "Arkanoid Copied!");
 	app.setFramerateLimit(60);
 
@@ -35,11 +42,22 @@ int main() {
 				app.close();
 		}
 
-		sBall.move(dx, dy);
+		sBall.move(dx, 0);
+		for(int i = 0; i < n; i++)
+			if (isCollide(sBall, block[i])) { block[i].setPosition(-100, 0); dx = -dx; }
+
+		sBall.move(0, dy);
+		for (int i = 0; i < n; i++)
+			if (isCollide(sBall, block[i])) { block[i].setPosition(-100, 0); dy = -dy; }
 
 		Vector2f b = sBall.getPosition();
 		if (b.x < 0 || b.x+10 > 520) dx = -dx;
 		if (b.y < 0 || b.y+10 > 450) dy = -dy;
+
+		if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-6, 0);
+
+		if (isCollide(sPaddle, sBall)) dy = -(rand() % 5 + 2);
 
 		app.clear();
 		app.draw(sBackground);
