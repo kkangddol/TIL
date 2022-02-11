f9e2393e4ec3f7a05f8bccb853ae1f960f83678e
KKANGDDOL <knemo333@gmail.com>
2022-02-11 17:13:25 +0900

    Update NowSolving.cpp

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
+	string str1, str2;
+	cin >> str1 >> str2;
 
+	int arr[1001][1001] = { 0, };
+
+	for (int i = 0; i < str1.size(); i++) {
+		for (int j = 0; j < str2.size(); j++) {
+			if (str2[j] == str1[i]) {
+				arr[i + 1][j + 1] = arr[i][j] + 1;
+			}
+			else {
+				arr[i + 1][j + 1] = max(arr[i][j + 1], arr[i + 1][j]);
+			}
+		}
+	}
+
+	cout << arr[str1.size()][str2.size()];
 }
