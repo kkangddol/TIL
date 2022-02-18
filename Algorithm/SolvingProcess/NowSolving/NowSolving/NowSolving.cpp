181426a14bc27193893147425785f148d3cec249
rudeore <knemo333@gmail.com>
2022-02-18 22:34:43 +0900

    평범한 배낭(냅색) 문제

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -1,27 +1,6 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
 using namespace std;
 
 int main() {
-	int n;
-	int arr[100000] = { 0, };
-	vector<int> memo;
-	
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
 
-	for (int i = 0; i < n; i++) {
-		if (memo.size() == 0) {
-			memo.push_back(arr[i]);
-			continue;
-		}
-		if (memo[i - 1] > 0)
-			memo.push_back(memo[i - 1] + arr[i]);
-		else {
-			memo.push_back(arr[i]);
-		}
-	}
-	cout << *max_element(memo.begin(), memo.end());
 }
