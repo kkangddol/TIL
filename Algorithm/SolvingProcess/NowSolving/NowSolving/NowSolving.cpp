cddfc282d2c0e5f876dcb434c27ec5b1113fc449
rudeore <knemo333@gmail.com>
2022-03-20 00:05:10 +0900

    주유소 완료

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 	int n;
-	int distances[100000];
-	int gasPrice[100000];
+	int distances[1000] = {0,};
+	int gasPrice[1000] = {0,};
 
 	cin >> n;
 	for (int i = 0; i < n-1; i++)
@@ -13,5 +13,21 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> gasPrice[i];
 
+	int ans = 0;
+	int distance = 0;
+	int minPrice = 1000000001;
 
+	for (int i = 0; i < n - 1; i++) {
+		if (minPrice <= gasPrice[i]) {
+			distance += distances[i];
+		}
+		else {
+			ans += minPrice * distance;
+			minPrice = gasPrice[i];
+			distance = distances[i];
+		}
+	}
+	ans += minPrice * distance;
+
+	cout << ans;
 }
