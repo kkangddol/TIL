3ab6178084cd9c408c1961234188fb4c244d21c0
rudeore <knemo333@gmail.com>
2022-03-21 21:39:13 +0900

    응 띄어쓰기

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -3,23 +3,18 @@
 using namespace std;
 
 int main() {
-	bool isEnd = false;
-	while (!isEnd) {
+	while (1) {
 		int num1 = 0, num2 = 0;
 		cin >> num1 >> num2;
 
-		if (num1 == 0 && num2 == 0) {
-			isEnd = true;
+		if (num1 == 0 && num2 == 0)
 			break;
-		}
 
-		if (num1 == 0 || num2 == 0)
-			cout << "neither";
-		else if (num2 % num1 == 0)
-			cout << "factor";
+		if (num2 % num1 == 0)
+			cout << "factor" << endl;
 		else if (num1 % num2 == 0)
-			cout << "multiple";
+			cout << "multiple" << endl;
 		else
-			cout << "neither";
+			cout << "neither" << endl;
 	}
 }
