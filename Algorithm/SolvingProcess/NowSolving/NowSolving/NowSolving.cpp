cb13fe67a56850bd5fb5ed4ce4f135aeafe5fbe8
rudeore <knemo333@gmail.com>
2022-03-23 18:15:47 +0900

    최소공배수

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -3,33 +3,35 @@
 using namespace std;
 
 int main() {
-	int a, b;
-	vector<int> primes;
+	int t;
+	cin >> t;
 
-	cin >> a >> b;
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		vector<int> primes;
 
-	int prime = 2;
-	while (1) {
-		if (prime > a || prime > b)
-			break;
+		cin >> a >> b;
 
-		if (a % prime == 0 && b % prime == 0) {
-			a /= prime;
-			b /= prime;
-			primes.push_back(prime);
-			prime = 2;
+		int prime = 2;
+		while (1) {
+			if (prime > a || prime > b)
+				break;
+
+			if (a % prime == 0 && b % prime == 0) {
+				a /= prime;
+				b /= prime;
+				primes.push_back(prime);
+				prime = 2;
+			}
+			else
+				prime++;
 		}
-		else
-			prime++;
+		int LCM = a * b;
+
+		vector<int>::iterator iter;
+		for (iter = primes.begin(); iter != primes.end(); iter++)
+			LCM *= (*iter);
+
+		cout << LCM << endl;
 	}
-	int GCD = 1;
-	int LCM = 1;
-
-	vector<int>::iterator iter;
-	for (iter = primes.begin(); iter != primes.end(); iter++)
-		GCD *= (*iter);
-
-	LCM = GCD * a * b;
-
-	cout << GCD << endl << LCM;
 }
