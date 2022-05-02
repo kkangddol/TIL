6277a4e386edd758dba1b8449aa850c4da83bf63
KKANGDDOL <knemo333@gmail.com>
2022-05-02 17:27:24 +0900

    검문 풀이 완료.
    유클리드 호제법 및 공약수, 최대공약수의 성질.

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -1,19 +1,62 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main() {
-	int n;
-	vector<int> numbers;
-	vector<int> m;
-	cin >> n;
+int GCD(int x, int y)
+{
+	if (x % y == 0)
+	{
+		return y;
+	}
+	else
+	{
+		return GCD(y, x % y);
+	}
+}
 
-	for (int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-		numbers.push_back(temp);
+int main()
+{
+	int n;
+	int inputArr[101] = { 0, };
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> inputArr[i];
+	}
+	sort(inputArr, inputArr + n);
+
+	int gcd = inputArr[1] - inputArr[0];
+	for (int i = 0; i < n - 1; i++)
+	{
+		gcd = GCD(inputArr[i + 1] - inputArr[i], gcd);
 	}
 
-	int temp = 2;
-	while()
+	vector<int> gcd_vector;
+	for (int i = 1; i <= gcd; i++)
+	{
+		if (gcd % i == 0) {
+			if (i > gcd / i) break;
+
+			gcd_vector.push_back(i);
+
+			if (i == gcd / i) break;
+
+			gcd_vector.push_back(gcd / i);
+		}
+	}
+
+	sort(gcd_vector.begin(), gcd_vector.end());
+
+	vector<int>::iterator iter = gcd_vector.begin();
+	for (iter; iter != gcd_vector.end(); iter++)
+	{
+		if (*iter != 1)
+		{
+			cout << *iter << endl;
+		}
+	}
+
+	return 0;
 }
