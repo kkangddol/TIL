2dd1c39201d3da2561c26f80dccf18f91285dcf3
SeokWon Kang <knemo333@gmail.com>
2022-05-04 14:08:03 +0900

    문제풀이 하나

--- Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
+++ Algorithm/SolvingProcess/NowSolving/NowSolving/NowSolving.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -19,43 +17,18 @@ int GCD(int x, int y)
 int main()
 {
 	int n;
-	int inputArr[101] = { 0, };
 	cin >> n;
+
+	int arr[101];
 	for (int i = 0; i < n; i++)
 	{
-		cin >> inputArr[i];
-	}
-	sort(inputArr, inputArr + n);
-
-	int gcd = inputArr[1] - inputArr[0];
-	for (int i = 0; i < n - 1; i++)
-	{
-		gcd = GCD(inputArr[i + 1] - inputArr[i], gcd);
+		cin >> arr[i];
 	}
 
-	vector<int> gcd_vector;
-	for (int i = 1; i <= gcd; i++)
+	for (int i = 1; i < n; i++)
 	{
-		if (gcd % i == 0) {
-			if (i > gcd / i) break;
-
-			gcd_vector.push_back(i);
-
-			if (i == gcd / i) break;
-
-			gcd_vector.push_back(gcd / i);
-		}
-	}
-
-	sort(gcd_vector.begin(), gcd_vector.end());
-
-	vector<int>::iterator iter = gcd_vector.begin();
-	for (iter; iter != gcd_vector.end(); iter++)
-	{
-		if (*iter != 1)
-		{
-			cout << *iter << endl;
-		}
+		int gcd = GCD(arr[0], arr[i]);
+		cout << arr[0] / gcd << "/" << arr[i] / gcd << endl;
 	}
 
 	return 0;
